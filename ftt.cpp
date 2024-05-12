452a44908aa849e74cf2afc950a08a1ddb0900e8
Ismail Shareef <S3967638@student.rmit.edu.au>
2024-05-12 22:54:19 +1000

    implemented printMainMenu() method

--- ftt.cpp
+++ ftt.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 #include "LinkedList.h"
 
+using std::string;
+using std::cout;
+using std::endl;
+using std::cin;
+
+void printMainMenu();
+
 /**
  * manages the running of the program, initialises data structures, loads
  * data, display the main menu, and handles the processing of options. 
@@ -11,7 +18,27 @@ int main(int argc, char **argv)
     /* validate command line arguments */
     // TODO
 
-    std::cout << "Just a test, nothing implemented yet!" << std::endl;
-    
+    cout << "Just a test, nothing implemented yet!" << endl;
+
     return EXIT_SUCCESS;
 }
+
+
+void printMainMenu() {
+
+    cout << "Main Menu:" << endl;
+
+    cout << "\t1. Display Meal Options" << endl;
+    cout << "\t2. Purchase Meal" << endl;
+    cout << "\t3. Save and Exit" << endl;
+
+    cout << "Administrator-Only Menu:" << endl;
+    cout << "\t4. Add Food" << endl;
+    cout << "\t5. Remove Food" << endl;
+    cout << "\t6. Display Balance" << endl;
+    cout << "\t7. Abort Program" << endl;
+
+    cout << "Select your option (1-7) :" << endl;
+
+}
+
